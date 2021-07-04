08789d0cc38d41c90a447efb85259af1ed6efa32
Jaya <jaya@Jayas-Mac-mini.local>
2021-07-04 13:23:38 +0530

    index page changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <h1>Welcome to ReactJs Application</h1>
+        <h1>Welcome to ReactJs Application - deployed via aws Codepipeline</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
